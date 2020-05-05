7e5aa538bee0c5284e985ea13227b8e52e8d505e
kuso1327 <63327433+kuso1327@users.noreply.github.com>
2020-05-05 16:34:01 +0800

    第四章：屬性及封裝

---
+++ CSharp/Assets/Class4_PropArryEnum.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Class4_PropArryEnum : MonoBehaviour
+{
+    public Ship ship;
+
+    private void Start()
+    {
+        //設定屬性
+        ship.length = 99.9f;
+        //取得屬性
+        print("船的長度：" + ship.length);
+        
+        //設定唯讀屬性
+        //ship.width = 20.5f//錯誤
+        //取得唯讀屬性
+        print("船的寬度：" + ship.width);
+
+        ship.atk = 50.3f;
+        print("船的攻擊力：" + ship.atk);
+        
+    }
+
+}
---
+++ CSharp/Assets/Ship.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    //公開欄位：給任何人存取
+    //從其他類別取得或存放資料
+    public float speed = 20;
+
+    //私人欄位：僅限於此類別存取
+    //從其他類別無法存取或存放資料
+    private float weight = 50;
+
+    //問題：
+    //假設需要取得但是不能修改 - 屬性(權限)
+    //屬性語法
+    //修飾詞 類型 名稱 {存取權限}
+    //get 可以取得
+    //ste可以存放
+    //屬性不會顯示在屬性面板上(ODIN)-UnityEditor
+    //讀寫權限屬性
+    public float length { get; set; }
+
+
+    //企劃：寬度固定30不變
+    //封裝欄位：
+    //1.私人欄位
+    //2.唯讀屬性(return私人欄位)
+    private float _width = 30;
+    //唯讀屬性：不能設定值
+    public float width { get { return _width; } }
+
+    private float _height = 10.5f;
+    //=>(黏巴達Lambda)
+    //C#6版以上|Unity 2017 C# 7版
+    //C#改版8版
+    //get => _height;等同於get { return _heigth; }
+    public float Height { get => _height;}
+
+    private float lv = 3;
+    //企劃：攻擊力等於攻擊力*3
+    //封裝欄位：
+    //1.私人欄位
+    //2.透過屬性存取(return私人欄位){私人欄位 = value}
+    //value設定屬性時給予的值
+    private float _atk;
+    public float atk { get { return _atk; }set { _atk = value*lv; } }
+
+
+
+}
